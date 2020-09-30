877aa71b064bfef86b917570100a1a4e580455f5
Max <mdaiglevigneault@gmail.com>
2020-09-30 10:56:25 -0400

    test temporaire des DAOS dans ControleurCaptures

--- Captures/src/controleur/ControleurCaptures.java
+++ Captures/src/controleur/ControleurCaptures.java
@@ -1,7 +1,17 @@
 package controleur;
 
+import java.util.List;
+
 import com.sun.media.jfxmedia.logging.Logger;
 
+import donnee.AnneeDAO;
+import donnee.JourDAO;
+import donnee.MoisDAO;
+import donnee.SommaireDAO;
+import modele.Annee;
+import modele.Jour;
+import modele.Mois;
+import modele.Sommaire;
 import vue.VueCapture;
 import vue.VueCaptures;
 
@@ -10,6 +20,10 @@ public class ControleurCaptures extends Controleur{
 	public ControleurCaptures()
 	{
 		Logger.logMsg(Logger.INFO, "new ControleurCaptures()");
+		
+		//TODO Remove
+		testerLesDAO();
+		//
 	}
 
 	public void actionOuvrirCapture(int id) {
@@ -18,6 +32,38 @@ public class ControleurCaptures extends Controleur{
 		VueCapture.getInstance().getControleur().actionOuvrirCapture(id);
 		
 	}
-
-
+	
+	
+	//TODO remove
+	//TEST DES DAOS
+	
+	private void testerLesDAO() {
+		Logger.logMsg(Logger.INFO, "testerLesDAO => début");
+		testerJourDAO();
+		testerMoisDAO();
+		testerAnneeDAO();
+		Sommaire sommaire = SommaireDAO.getInstance().getSommaire();
+		Logger.logMsg(Logger.INFO, sommaire.toString());
+		Logger.logMsg(Logger.INFO, "testerLesDAO => fin");
+	}
+	
+	private void testerJourDAO() {
+		List<Jour> listejours = JourDAO.getInstance().getListe();
+		for (Jour jour : listejours) {
+			Logger.logMsg(Logger.INFO, jour.toString());
+		}
+	}
+	private void testerMoisDAO() {
+		List<Mois> listemois = MoisDAO.getInstance().getListe();
+		for (Mois mois : listemois) {
+			Logger.logMsg(Logger.INFO, mois.toString());
+		}
+	}
+	private void testerAnneeDAO() {
+		List<Annee> listeannee = AnneeDAO.getInstance().getListe();
+		for (Annee annee : listeannee) {
+			Logger.logMsg(Logger.INFO, annee.toString());
+		}
+	}
+	//
 }
